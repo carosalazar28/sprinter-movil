8c9f654f69225a7660cecc3f5479a1c17f1a474c
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-01-28 17:10:59 -0500

    change the function name

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { FormSignUp } from './components/formSignUp';
+import { SignUp } from './components/formSignUp';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Stack = createStackNavigator()
@@ -10,8 +10,12 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Registro" component={FormSignUp} />
+        <Stack.Navigator
+          screenOptions={{
+            cardStyle: { backgroundColor: '#2f2a3e'}
+          }}
+        >
+          <Stack.Screen name="Registro" component={SignUp} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
