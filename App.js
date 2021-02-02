bc4754176a9f559271e1705d3a8f3f86624c3866
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-02-02 14:47:36 -0500

    import one tab navigator

--- App.js
+++ App.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { SignUp } from './components/SignUp';
-import { SignIn } from './components/SignIn';
-import { Home } from './components/Home';
+import { BottomTabNavigator } from './components/TabNavigator';
 
 const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            cardStyle: { backgroundColor: '#2f2a3e'}
-          }}
-        >
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="SignIn" component={SignIn} />
-          <Stack.Screen name="SignUp" component={SignUp} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <BottomTabNavigator/>
+    </NavigationContainer>
   );
 }
