f3bf04478ba2539c4e81ac349d318a9a6d6173fc
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-02-02 15:20:07 -0500

    fix one import

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { BottomTabNavigator } from './components/TabNavigator';
 
-const Stack = createStackNavigator()
-
 export default function App() {
   return (
     <NavigationContainer>
