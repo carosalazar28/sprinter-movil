c3fa67278a74431ac64749f9d4bc4950f52dbc5d
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-02-05 16:38:54 -0500

    add template for component for create workspace

--- screens/Workspace.js
+++ screens/Workspace.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, Button, TextInput } from 'react-native';
+import { Text, Icon } from 'react-native-elements';
 import { Picker } from '@react-native-picker/picker';
 import {
   ViewContainerWorkspace,
@@ -9,6 +10,9 @@ import {
 export function Workspace({ navigation, route }) {
 
   const [sprint, setSprint] = useState(1);
+  const [description, setDescription] = useState('');
+  const [teammates, setTeammates] = useState([]);
+  const [weeks, setWeeks] = useState('');
 
   useEffect(() => {
     
@@ -35,6 +39,54 @@ export function Workspace({ navigation, route }) {
           </Picker>  
         </View>
       </ViewContainerSprint>
+      <View>
+        <TextInput
+          placeholder="Cual es la descripción del espacio de trabajo"
+          placeholderTextColor ="rgba(45, 42, 54, 1)"
+          onChangeText={text => setDescription(text)}
+          value={description}
+        />
+      </View>
+      <View>
+        <Text>Cuantas semanas dura el proyecto</Text>
+        <TextInput
+          placeholderTextColor ="rgba(45, 42, 54, 1)"
+          onChangeText={text => setweeks(text)}
+          value={weeks}  
+        />
+      </View>
+      <View>
+        <Text>Colaboradores</Text>
+        <TextInput
+          placeholderTextColor ="rgba(45, 42, 54, 1)"
+          onChangeText={text => setTeammates(text)}
+          value={teammates}
+        />
+        <Icon
+            name="adduser"
+            type="ant-design"
+            color="#525666"
+          />
+      </View>
+      <View>
+        <Icon
+          name="plus"
+          type="ant-design"
+          color="#69c8d4"
+          onPress={() => navigation.navigate('Workspace')}
+        />
+        <Text h3>Agregar backlog</Text>
+      </View>
+      <View>
+        <Button
+          title="Guardar"
+          color="#f2ea0d"
+        />
+        <Button
+          title="Cancel"
+          color="#bdbdbb"
+        />
+      </View>
     </ViewContainerWorkspace>
   )
 }
