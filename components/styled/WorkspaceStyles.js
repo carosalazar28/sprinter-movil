05c21e5d5b311d6021c20062317cb1881bebb390
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-02-05 10:59:11 -0500

    add file for style workspace

---
+++ components/styled/WorkspaceStyles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import { Text } from 'react-native-elements';
+import { 
+  View, 
+} from 'react-native';
+
+export const ViewContainer = styled(View)`
+  margin-top: 10px;
+`;
+
+export const Title = styled(Text)`
+  color: #f2ea0d;
+  margin: 10px 0;
+`;
+
+export const TextAbout = styled(Text)`
+  color: #fff;
+  font-size: 14px;
+`; 
+
+export const ContainerAbout = styled(View)`
+  padding: 17px;
+  width: 330px; 
+  height: 107px;
+  margin-bottom: 17px;
+`;
+
+export const ViewContainerWorkspaces = styled(View)`
+  background-color: #fff;
+  margin-top: 10px;
+  padding: 23px 0;
+`;
