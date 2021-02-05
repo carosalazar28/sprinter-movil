0882b26c6e554f6f5aadf5cd4302e8e5cb005039
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-02-05 11:00:38 -0500

    render main stack when does not have token

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { BottomTabNavigator } from './components/TabNavigator';
+import { MainStackNavigator } from './components/StackNavigator';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
+
+  const [token, setToken] = useState()
+
+  async function getToken() {
+    const token = await AsyncStorage.getItem('token')
+    setToken(token)
+  }
+  
+  useEffect(() => {
+    getToken()
+  }, [])
+
   return (
     <NavigationContainer>
-      <BottomTabNavigator/>
+      {token ? (
+        <BottomTabNavigator/>
+      ) : (
+        <MainStackNavigator/>
+      )}
     </NavigationContainer>
   );
 }
