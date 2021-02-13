7b929ef6014a5103c558dae0983070a309fa4c7b
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-02-13 18:03:22 -0500

    put the dispatch for read, update and destroy user

--- screens/ProfileEdit.js
+++ screens/ProfileEdit.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { View, Text, Image, StyleSheet, Button } from 'react-native';
 import {
   CustomInput,
 } from '../components/styled/FormStyles';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  getUser,
+  setUserName,
+  setEmail,
+  setRol,
+  updateUser,
+  destroyUser,
+} from '../store/actions/user.action';
 
 export function ProfileEdit() {
 
+  const dispatch = useDispatch();
+
+  const dataUser = useSelector((
+    { userReducer: {
+      ...state
+    }}) => {
+    return { ...state };
+  });
+
+  const { username, email, rol } = dataUser;
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, []);
+
   const handleClose = async () => {
     await AsyncStorage.removeItem('token');
+  };
+
+  const handleSubmit = (e) => {
+    dispatch(updateUser(dataUser));
+  };
+
+  const handleDelete = (e) => {
+    dispatch(destroyUser());
   };
 
   return (
@@ -29,34 +61,34 @@ export function ProfileEdit() {
         <CustomInput
           placeholder="Nombre de usuario"
           placeholderTextColor = "black"
-          onChangeText="{text => setUserName(text)}"
-          value="{username}"
+          onChangeText={text => setUserName(text)}
+          value={username}
         />
         <Text>Username</Text>
         <CustomInput
-          placeholder="Nombre de usuario"
+          placeholder="Email"
           placeholderTextColor = "black"
-          onChangeText="{text => setUserName(text)}"
-          value="{username}"
+          onChangeText={text => setEmail(text)}
+          value={email}
         />
         <Text>Rol</Text>
         <CustomInput
-          placeholder="Nombre de usuario"
+          placeholder="Rol"
           placeholderTextColor = "black"
-          onChangeText="{text => setUserName(text)}"
-          value="{username}"
+          onChangeText={text => setRol(text)}
+          value={rol}
         />
       </View>
       <View>
         <Button
           title="Guardar"
           color="#f2ea0d"
-          onPress="{handleSubmit}"
+          onPress={handleSubmit}
         />
         <Button
           title="Borrar"
           color="#bdbdbb"
-          onPress="{handleDelete}"
+          onPress={handleDelete}
         />
       </View>
     </>
