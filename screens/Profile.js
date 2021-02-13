ff127776ecfc15b58c06861abe5221ddaca056a0
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-02-13 11:59:06 -0500

    fix the component profile

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { ListItem, Icon } from 'react-native-elements';
+
+const list = [
+  {
+    title: 'Backlog',
+    icon: 'list-alt',
+    type: 'font-awesome'
+  },
+  {
+    title: 'Tareas asignadas',
+    icon: 'folder-open',
+    type: 'font-awesome'
+  },
+];
 
 export function Profile() {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <>
+      <View>
+        <Image
+          source={{ uri: 'https://res.cloudinary.com/dkcbxnhg0/image/upload/v1613234403/sprinter/ui/Logo_SPRINTER_vpejk2.png' }}
+          style={{ width: 80, height: 75, borderRadius: 25 }}
+        />
+        <Text>Carolina</Text>
+      </View>
+      <View>
+        { list.map((item, i) => (
+          <ListItem key={i} bottomDivider>
+            <Icon name={item.icon} type={item.type} />
+            <ListItem.Content>
+              <ListItem.Title>{item.title}</ListItem.Title>
+            </ListItem.Content>
+            <ListItem.Chevron />
+          </ListItem>
+        ))}
+      </View>
+    </>
   );
 }
