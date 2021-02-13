da4f5582b3ecaec14d79c7a9a6e47e006dead578
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-02-13 16:51:09 -0500

    add component for logout

--- screens/ProfileEdit.js
+++ screens/ProfileEdit.js
@@ -3,14 +3,25 @@ import { View, Text, Image, StyleSheet, Button } from 'react-native';
 import {
   CustomInput,
 } from '../components/styled/FormStyles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export function ProfileEdit() {
+
+  const handleClose = async () => {
+    await AsyncStorage.removeItem('token');
+  };
+
   return (
     <>
       <View>
         <Image
           source={{ uri: 'https://res.cloudinary.com/dkcbxnhg0/image/upload/v1613234403/sprinter/ui/Logo_SPRINTER_vpejk2.png' }}
           style={{ width: 180, height: 180, marginTop: 30, marginBottom: 15 }}
+        />
+        <Button
+          title="Cerrar Sesión"
+          color="#bdbdbb"
+          onPress={handleClose}
         />
       </View>
       <View>
